a69d5c53ca9fb09f894546baf58399186eed7017
bingle625 <fhqhxm8899@gmail.com>
2025-02-01 12:39:24 +0900

    Initial commit

---
+++ src/main/java/org/gdsccau/team5/safebridge/SafeBridgeApplication.java
@@ -0,0 +1,13 @@
+package org.gdsccau.team5.safebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBridgeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SafeBridgeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/gdsccau/team5/safebridge/SafeBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdsccau.team5.safebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeBridgeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
