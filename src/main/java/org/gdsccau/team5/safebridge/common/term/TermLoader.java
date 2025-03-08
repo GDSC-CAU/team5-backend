730a0b384150bb9bb8603183bd279e8e47a6788b
bingle625 <fhqhxm8899@gmail.com>
2025-03-08 21:44:41 +0900

    fix: 빌드 시 코드상에서 물리 파일 path 에 접근되지 않는 오류 수정

--- src/main/java/org/gdsccau/team5/safebridge/common/term/TermLoader.java
+++ src/main/java/org/gdsccau/team5/safebridge/common/term/TermLoader.java
@@ -6,13 +6,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.springframework.core.io.ClassPathResource;
 
 public class TermLoader {
 
     public static Map<String, String> loadTermsWithMeaning() {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            File file = new File("src/main/resources/terms_with_meaning.json");
+            File file = new ClassPathResource("terms_with_meaning.json").getFile();
             return objectMapper.readValue(file, HashMap.class);
         } catch (Exception e) {
             e.printStackTrace();
@@ -23,7 +24,7 @@ public class TermLoader {
     public static Set<String> loadTermsOnly() {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            File file = new File("src/main/resources/terms_only.json");
+            File file = new ClassPathResource("terms_only.json").getFile();
             List<String> terms = objectMapper.readValue(file, List.class);
             return Set.copyOf(terms);
         } catch (Exception e) {
