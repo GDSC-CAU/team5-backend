623999c6b744a13d1bb928ffcc100f34429676ab
bingle625 <fhqhxm8899@gmail.com>
2025-03-09 14:53:47 +0900

    fix: enum 은 notblank 안되는 이슈

--- src/main/java/org/gdsccau/team5/safebridge/domain/user/dto/UserRequestDto.java
+++ src/main/java/org/gdsccau/team5/safebridge/domain/user/dto/UserRequestDto.java
@@ -1,13 +1,9 @@
 package org.gdsccau.team5.safebridge.domain.user.dto;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.gdsccau.team5.safebridge.common.term.Language;
-import org.gdsccau.team5.safebridge.domain.user.entity.User;
 
 @Builder
 public record UserRequestDto(
@@ -19,7 +15,7 @@ public record UserRequestDto(
   }
 
   public record UserSignUpDto(@NotBlank(message = "이름은 필수입니다.") String name,
-                              @NotBlank(message = "언어선택은 필수입니다!") Language language,
+                              @NotNull(message = "언어선택은 필수입니다!") Language language,
                               @NotBlank(message = "로그인 아이디는 필수입니다!") String loginId,
                               @NotBlank(message = "비밀번호 입력은 필수입니다!") String password) {
 
